f5bdf56ad52e5be4e8493fc6ecb7a35bb94d03fb
Rado Klein <klein.radoslav.1992@gmail.com>
2022-08-26 16:21:28 +0200

    filip did this

--- js.js
+++ js.js
@@ -1,33 +1,39 @@
 // RADO and Maros Java Script madafaka
 //
+function rado() {
+  console.log(5);
+  
+}
+
 let k = "The respective values are :";
-let input = [10,50,40,50,80,90,70];
-  function Geeks() {            
-    //let input = document.getElementsByName('array[]'); 
-      for (let i = 0; i < input.length; i++) {
-          let a = input[i];
+let input = document.querySelectorAll(".input--num");
+//let input = [10,50,40,50,80,90,70];
+  function Geeks() {          
+        for (let i = 0; i < input.length; i++) {
+          let a = input[i].value;
           k += "array[" + i + "].value = "+ a + " ";
         }
         return k;
-  //document.getElementById("par").innerHTML = k;
+  //document.getElementsByClassName("result").innerHTML = k;
   //document.getElementById("po").innerHTML = "Output";
       }
 
 function min(){
-let min = input[0];
+let min = parseFloat(input[0].value);
 for (let i = 0; i < input.length; i++){
-  if (input[i] < min) {
-    min = input[i];
+  if (input[i].value < min) {
+    min = parseFloat(input[i].value);
   }
 }
+console.log(min)
 return min;
 }
 
 function max(){
-  let max = input[0];
+  let max = parseFloat(input[0].value);
   for (let i = 0; i < input.length; i++){
-    if (input[i] > max) {
-      max = input[i];
+    if (input[i].value > max) {
+      max = parseFloat(input[i].value);
     }
   }
   return max;
@@ -36,7 +42,7 @@ function max(){
 function sum(){
   let summary = 0;
   for (let i = 0; i < input.length; i++){
-    summary += input[i];
+    summary += parseFloat(input[i].value);
   }
   return summary;
 }
@@ -50,10 +56,28 @@ function sum(){
 
 
 
-console.log(min());
-console.log(max());
-console.log(input.length);
-console.log(sum());
-console.log(avg());
-console.log(Geeks());
+  function calc(){
+
+    // Geeks(); min(); max(); avg(); sum(); rado();
+
+    document.querySelector(".min").innerHTML = Geeks();
+    document.querySelector(".min").innerHTML = min();
+    document.querySelector(".max").innerHTML = max();
+    document.querySelector(".avg").innerHTML = avg();
+    document.querySelector(".sum").innerHTML = sum();
+    
+    
+    
+    console.log(min());
+    console.log(max());
+    console.log(input.length);
+    console.log(sum());
+    console.log(avg());
+    console.log(Geeks());
+
+  }
+
+
+
+
           

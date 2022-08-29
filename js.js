d884302c44ca239250bdeafa431e7567f11fba19
Maros Laso <maros.laso@gmail.com>
2022-08-29 13:22:17 +0200

    Zmenene html,css aj js, popridavane komentare

--- js.js
+++ js.js
@@ -2,82 +2,76 @@
 //
 function rado() {
   console.log(5);
-  
 }
 
 let k = "The respective values are :";
 let input = document.querySelectorAll(".input--num");
 //let input = [10,50,40,50,80,90,70];
-  function Geeks() {          
-        for (let i = 0; i < input.length; i++) {
-          let a = input[i].value;
-          k += "array[" + i + "].value = "+ a + " ";
-        }
-        return k;
+function Geeks() {
+  for (let i = 0; i < input.length; i++) {
+    let a = input[i].value;
+    k += "array[" + i + "].value = " + a + " ";
+  }
+  return k;
   //document.getElementsByClassName("result").innerHTML = k;
   //document.getElementById("po").innerHTML = "Output";
-      }
+}
 
-function min(){
-let min = parseFloat(input[0].value);
-for (let i = 0; i < input.length; i++){
-  if (input[i].value < min) {
-    min = parseFloat(input[i].value);
+function min() {
+  let min = parseFloat(input[0].value);
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].value < min) {
+      min = parseFloat(input[i].value);
+    }
   }
-}
-console.log(min)
-return min;
+  document.querySelector(".min").textContent = min;
+  return min;
 }
 
-function max(){
+function max() {
   let max = parseFloat(input[0].value);
-  for (let i = 0; i < input.length; i++){
+  for (let i = 0; i < input.length; i++) {
     if (input[i].value > max) {
       max = parseFloat(input[i].value);
     }
   }
+  document.querySelector(".max").textContent = max;
   return max;
 }
 
-function sum(){
+function sum() {
   let summary = 0;
-  for (let i = 0; i < input.length; i++){
+  for (let i = 0; i < input.length; i++) {
     summary += parseFloat(input[i].value);
   }
+  document.querySelector(".sum").textContent = summary;
   return summary;
 }
 
-  function avg(){
-    let avg_val = 0;    
-    avg_val = sum() / input.length;
-    avg_val = Math.round(avg_val);
-    return avg_val;
-  }  
+function avg() {
+  let avg_val = 0;
+  avg_val = sum() / input.length;
+  avg_val = Math.round(avg_val);
 
+  document.querySelector(".avg").textContent = avg_val;
+  return avg_val;
+}
 
+//!!! Vlozil som to do kazdej funkcie aby to zapisovalo, pozri pri kazdej fukncii na alternativne riesenie :)
 
-  function calc(){
+// function calc() {
+//   // Geeks(); min(); max(); avg(); sum(); rado();
 
-    // Geeks(); min(); max(); avg(); sum(); rado();
+//   // document.querySelector(".min").innerHTML = Geeks();
+//   document.querySelector(".min").innerHTML = min();
+//   document.querySelector(".max").innerHTML = max();
+//   document.querySelector(".avg").innerHTML = avg();
+//   document.querySelector(".sum").innerHTML = sum();
 
-    document.querySelector(".min").innerHTML = Geeks();
-    document.querySelector(".min").innerHTML = min();
-    document.querySelector(".max").innerHTML = max();
-    document.querySelector(".avg").innerHTML = avg();
-    document.querySelector(".sum").innerHTML = sum();
-    
-    
-    
-    console.log(min());
-    console.log(max());
-    console.log(input.length);
-    console.log(sum());
-    console.log(avg());
-    console.log(Geeks());
-
-  }
-
-
-
-
-          
+//   console.log(min());
+//   console.log(max());
+//   console.log(input.length);
+//   console.log(sum());
+//   console.log(avg());
+//   console.log(Geeks());
+// }

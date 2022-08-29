d29a273556f79f797bde83a426eda41ca6382a1d
Rado Klein <klein.radoslav.1992@gmail.com>
2022-08-29 17:33:20 +0200

    rado did this

--- js.js
+++ js.js
@@ -4,8 +4,42 @@ function rado() {
   console.log(5);
 }
 
+//aby sme nemusli vzdy zadavat novo ciselka tak stalcis random button, kt. by si mohol vytvorit a niekde ho pekne vlozit :D 
+function randomarr(){  
+  let zero = [];
+zero[0] = document.querySelector("#val_0_");
+zero[1] = document.querySelector("#val_1_");
+zero[2] = document.querySelector("#val_2_");
+zero[3] = document.querySelector("#val_3_");
+zero[4] = document.querySelector("#val_4_");
+zero[5] = document.querySelector("#val_5_");
+zero[6] = document.querySelector("#val_6_");
+zero[7] = document.querySelector("#val_7_");
+zero[8] = document.querySelector("#val_8_");
+zero[9] = document.querySelector("#val_9_");
+  let newarr = [];   
+  while(newarr.length < 10){
+      let r = Math.floor(Math.random() * 1000) + 1;
+      if(newarr.indexOf(r) === -1) newarr.push(r);
+  }
+  //input = newarr;
+  //console.log(newarr);
+  //console.log(input);
+  // for (let i = 0; i < 10; i++){
+  //   document.getElementById("val_" + i + "_").value = input[i];
+  // } 
+
+for(let i = 0; i < 10; i++){
+  zero[i] = newarr[i];
+}
+console.log(zero);
+  return newarr;  
+}  
+
 let k = "The respective values are :";
 let input = document.querySelectorAll(".input--num");
+
+//
 //let input = [10,50,40,50,80,90,70];
 function Geeks() {
   for (let i = 0; i < input.length; i++) {
@@ -18,13 +52,14 @@ function Geeks() {
 }
 
 function min() {
+  console.log(input);
   let min = parseFloat(input[0].value);
   for (let i = 0; i < input.length; i++) {
     if (input[i].value < min) {
       min = parseFloat(input[i].value);
     }
   }
-  document.querySelector(".min").textContent = min;
+    // fuj document.querySelector(".min").textContent = min;
   return min;
 }
 
@@ -35,7 +70,7 @@ function max() {
       max = parseFloat(input[i].value);
     }
   }
-  document.querySelector(".max").textContent = max;
+    // fuj document.querySelector(".max").textContent = max;
   return max;
 }
 
@@ -44,7 +79,7 @@ function sum() {
   for (let i = 0; i < input.length; i++) {
     summary += parseFloat(input[i].value);
   }
-  document.querySelector(".sum").textContent = summary;
+  // fuj document.querySelector(".sum").textContent = summary;
   return summary;
 }
 
@@ -53,25 +88,26 @@ function avg() {
   avg_val = sum() / input.length;
   avg_val = Math.round(avg_val);
 
-  document.querySelector(".avg").textContent = avg_val;
+  // fuj document.querySelector(".avg").textContent = avg_val;
   return avg_val;
 }
 
 //!!! Vlozil som to do kazdej funkcie aby to zapisovalo, pozri pri kazdej fukncii na alternativne riesenie :)
 
-// function calc() {
-//   // Geeks(); min(); max(); avg(); sum(); rado();
+function calc() {
+   Geeks(); min(); max(); avg(); sum(); //rado();
 
-//   // document.querySelector(".min").innerHTML = Geeks();
-//   document.querySelector(".min").innerHTML = min();
-//   document.querySelector(".max").innerHTML = max();
-//   document.querySelector(".avg").innerHTML = avg();
-//   document.querySelector(".sum").innerHTML = sum();
+  document.querySelector(".result").innerHTML = Geeks();
+  document.querySelector(".min").innerHTML = min();
+  document.querySelector(".max").innerHTML = max();
+  document.querySelector(".avg").innerHTML = avg();
+  document.querySelector(".sum").innerHTML = sum();
 
-//   console.log(min());
-//   console.log(max());
-//   console.log(input.length);
-//   console.log(sum());
-//   console.log(avg());
-//   console.log(Geeks());
-// }
+  console.log(min());
+  console.log(max());
+  console.log(input.length);
+  console.log(sum());
+  console.log(avg());
+  console.log(Geeks());
+}
+
---
+++ test.js
@@ -0,0 +1,11 @@
+
+// function randomarr(){   
+//     let newarr = [];  
+//   while(newarr.length < 10){
+//       let r = Math.floor(Math.random() * 1000) + 1;
+//       if(newarr.indexOf(r) === -1) newarr.push(r);
+//   }
+//   return newarr;
+// }
+  
+//   console.log(randomarr());
